4387214e83464f7adff7116de2a0cfff808c652d
carbon-ahs <shehanuk.ahsan@gmail.com>
2021-07-13 20:37:03 +0600

    api_request has been set up

--- quotes/views.py
+++ quotes/views.py
@@ -2,10 +2,28 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+    # pk_4795a3382d6442558cd97acafe4b28ec 
+    # https://cloud.iexapis.com/stable/stock/aapl/quote/?token=pk_4795a3382d6442558cd97acafe4b28ec
+    import requests
+    import json
+
+    api_request = requests.get('https://cloud.iexapis.com/stable/stock/aapl/quote/?token=pk_4795a3382d6442558cd97acafe4b28ec')
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = 'Error...'
+
+    contex = {
+        'key': 'value',
+        'api': api,
+    }
+
+    return render(request, 'home.html', contex)
+
+def about(request):
 
     contex = {
         'key': 'value',
     }
-
-    return render(request, 'home.html', contex)
+    return render(request, 'about.html', contex)
 
